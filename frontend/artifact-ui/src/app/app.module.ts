77d4f62820da109db0e3ab5d526d767e1948b495
Alvaro Ruiz Zamora <arz641@inlumine.ual.es>
2019-05-24 17:58:02 +0200

    language selector

--- frontend/artifact-ui/src/app/app.module.ts
+++ frontend/artifact-ui/src/app/app.module.ts
@@ -7,10 +7,11 @@ import { AppComponent } from "./app.component";
 import { GotNavBarComponent } from "./got-nav-bar/got-nav-bar.component";
 import { MemberListComponent } from './member-list/member-list.component';
 import {MatCardModule} from '@angular/material/card';
+import { LanguageSelectorComponent } from './language-selector/language-selector.component';
 
 
 @NgModule({
-  declarations: [AppComponent, GotNavBarComponent, MemberListComponent],
+  declarations: [AppComponent, GotNavBarComponent, MemberListComponent, LanguageSelectorComponent],
   imports: [BrowserModule, AppRoutingModule, MatInputModule,MatCardModule],
   bootstrap: [AppComponent]
 })
