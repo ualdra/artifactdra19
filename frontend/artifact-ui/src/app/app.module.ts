63ce53207316ffe0c50b3e11bc7eb7d970a424be
Alvaro Ruiz Zamora <arz641@inlumine.ual.es>
2019-05-24 18:15:28 +0200

    tarea #58

--- frontend/artifact-ui/src/app/app.module.ts
+++ frontend/artifact-ui/src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { MatInputModule } from "@angular/material/input";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { GotNavBarComponent } from "./got-nav-bar/got-nav-bar.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GotNavBarComponent } from './got-nav-bar/got-nav-bar.component';
 import { MemberListComponent } from './member-list/member-list.component';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
+import { HttpClientModule } from '@angular/common/http';
+import { CardListItemComponent } from 'src/app/card-list-item/card-list-item.component';
+import { CardListComponent } from 'src/app/card-list/card-list.component';
+import { CardListService } from 'src/app/services/cardList.service';
 import { LanguageSelectorComponent } from './language-selector/language-selector.component';
 
 
 @NgModule({
-  declarations: [AppComponent, GotNavBarComponent, MemberListComponent, LanguageSelectorComponent],
-  imports: [BrowserModule, AppRoutingModule, MatInputModule,MatCardModule],
+  declarations: [AppComponent, GotNavBarComponent, MemberListComponent, 
+    CardListItemComponent, CardListComponent, LanguageSelectorComponent],
+  imports: [BrowserModule, AppRoutingModule, MatInputModule, MatCardModule, HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
